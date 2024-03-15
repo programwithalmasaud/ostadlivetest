d9b183acf64678f4d44e5bbd620302ac6d4cc659
programwithalmasaud <almasaud1999@gmail.com>
2024-03-15 23:34:39 +0600

    Initial commit

---
+++ bin/ostadlivetest.dart
@@ -0,0 +1,36 @@
+import 'package:ostadlivetest/ostadlivetest.dart' as ostadlivetest;
+
+
+void main() {
+  List<Map<String, dynamic>> fruits = [
+    {"name": "Apple", "color": "Red", "price": 2.5},
+    {"name": "Banana", "color": "Yellow", "price": 1.0},
+    {"name": "Grapes", "color": "Purple", "price": 3.0},
+    {"name": "Orange ", "color": "Orange", "price": 4.0},
+    {"name": "Mango", "color": "Yellow", "price": 5.0}
+    
+
+  ];
+
+  print("Original Fruit Details before Discount:");
+  displayFruitDetails(fruits);
+
+  applyPriceDiscount(fruits, 10);
+
+  print("\nFruit Details After Applying 10% Discount:");
+  displayFruitDetails(fruits);
+}
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  for (var fruit in fruits) {
+    print("Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price'].toStringAsFixed(2)}");
+  }
+}
+
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discountPercentage) {
+  for (var fruit in fruits) {
+    double originalPrice = fruit['price'];
+    double discountAmount = originalPrice * (discountPercentage / 100);
+    fruit['price'] = originalPrice - discountAmount;
+  }
+}
---
+++ test/ostadlivetest_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostadlivetest/ostadlivetest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
